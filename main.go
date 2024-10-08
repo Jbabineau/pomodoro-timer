33fa78c93b6c8e16dcf215ba726dc954642ff0f6
Jamie Babineau <babineau.james@gmail.com>
2024-10-08 00:35:41 -0400

    adding templ for index and hello

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/jbabineau/pomodoro-timer/templates"
 )
 
 func main() {
 
 	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("Server Started and listening on :8181")
 	log.Fatal(http.ListenAndServe(":8181", nil))
@@ -17,4 +20,16 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Ping Handler hit")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Pong"))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	page := templates.Index("world")
+
+	err := page.Render(r.Context(), w)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 }
